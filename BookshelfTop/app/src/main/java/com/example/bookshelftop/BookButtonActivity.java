a94e8d428b5ecf5c8e6fcb47121f1dfeab17b984
nandsfordays <42986987+nandsfordays@users.noreply.github.com>
2018-11-29 09:30:41 -0600

    Add ability to swipe

--- BookshelfTop/app/src/main/java/com/example/bookshelftop/BookButtonActivity.java
+++ BookshelfTop/app/src/main/java/com/example/bookshelftop/BookButtonActivity.java
@@ -1,12 +1,17 @@
 package com.example.bookshelftop;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+import android.support.v4.view.ViewPager;
 import android.widget.TextView;
 
-public class BookButtonActivity extends AppCompatActivity {
-
+public class BookButtonActivity extends FragmentActivity {
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +22,15 @@ public class BookButtonActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(BookShelfActivity.EXTRA_MESSAGE);
 
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+        // Capture the layout's ViewPager and view with swipes
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+
+
+        PageNavigator pageNavigator = new PageNavigator(new DocGrabber().readFileFromDirectory(message));
+        SwipeAdapter swipeAdapter = new SwipeAdapter(getSupportFragmentManager(),pageNavigator);
+        viewPager.setAdapter(swipeAdapter);
     }
+
+
+
 }
