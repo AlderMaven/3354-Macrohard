4c0f208aaefcca6c3ce8be43447cdddf9cef0b46
radicalduck <shahs135@gmail.com>
2018-12-06 15:54:22 -0600

    Pages work now, book text shows up on screen

--- BookshelfTop/app/src/main/java/com/example/bookshelftop/BookButtonActivity.java
+++ BookshelfTop/app/src/main/java/com/example/bookshelftop/BookButtonActivity.java
@@ -1,6 +1,7 @@
 package com.example.bookshelftop;
 
 import android.content.ContextWrapper;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.File;
@@ -27,9 +29,9 @@ public class BookButtonActivity extends FragmentActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(BookShelfActivity.EXTRA_MESSAGE);
 
-        //ss stuff------------------------------------------
-
+        //ss stuff-----------------------------------------
         text_holder = (TextView)findViewById(R.id.textHolder);  //write text to this
+        text_holder.setOnClickListener(goToNextPage);
         ContextWrapper c = new ContextWrapper(this);
         File bFile = new File(c.getFilesDir(), message);       //file with text (hopefully)
 
@@ -62,14 +64,21 @@ public class BookButtonActivity extends FragmentActivity {
         */
     }
 
-    void make_and_viewPage(FileReader bReader, long startLoc, TextView text_holder)
+    private View.OnClickListener goToNextPage = new View.OnClickListener() {
+        public void onClick(View v) {
+            make_and_viewPage(bReader, 0, text_holder);
+        }
+    };
+
+    void make_and_viewPage(FileReader bReader, int startLoc, TextView text_holder)
     {
         String pageText = "";
         int temp = -1;
         try {
-            for (int pos = 0; pos < 100; pos++)  //reads 100 characters
+            for (int pos = 0; pos < 1500; pos++)  //reads 100 characters
             {
-                while ((temp = bReader.read()) != -1) {  //temp is int, need to cast to a char to use
+                if ((temp = bReader.read()) != -1) {  //temp is int, need to cast to a char to use
+
                     pageText = pageText + (char)temp;
                 }
             }
