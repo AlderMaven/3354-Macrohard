4bddb6b0feef8daa3ed5392e661cd82b460c5c6c
Alder Maven <43149194+AlderMaven@users.noreply.github.com>
2018-12-06 10:45:20 -0600

    Finalized JavaDoc and Comments for DocGrabber

--- BookshelfTop/app/src/main/java/com/example/bookshelftop/DocGrabber.java
+++ BookshelfTop/app/src/main/java/com/example/bookshelftop/DocGrabber.java
@@ -39,14 +39,30 @@ public class DocGrabber extends AppCompatActivity {
     }
 
     //Is called when focus leaves textbox
+    /**
+    * 1st call that resolves for importing a book
+    * which will attempt to make a copy of a file from the downloads directory and place it into the app's local directory
+    * If successful it will also add that book's file name to the assets.dat file in the local directory
+    *
+    *
+    * @param view the view of the current text model
+    *
+    *
+    * @author Brandon Barnes
+    *
+    *
+    */
     public void onEnter(View view) {
         Intent intent = new Intent(this, DocGrabber.class); //Probably removable
 
         //Grab file name from textline
         EditText editText = (EditText) findViewById(R.id.FileNameBox);
         String bookFileName = editText.getText().toString();
+
+
         boolean notImported = false;
         boolean notInvalid = false;
+
         //Just for returning messages
         String onSuccess = "File imported successfully";
         String onFail = "Could not import file";
@@ -83,14 +99,16 @@ public class DocGrabber extends AppCompatActivity {
 
         }
         catch(Exception e) {
-
+            //This will trigger if reading from assets.dat occurs
+            notImported = false; //Treating cases where book may or not be imported already as if it is already imported
         }
+        //Check to ensure user cannot overwrite assets.dat
         if(bookFileName.equals(assetsFileName)) {
             notInvalid = false;
             editText.setText(onInvalid);
         }
         else {
-            notInvalid = true;
+            notInvalid = true; //bookFileName is not assets,dat
         }
         if(notInvalid && notImported) {
             //Call function and if add was successful will return true
@@ -101,7 +119,7 @@ public class DocGrabber extends AppCompatActivity {
             if (successful) { //if successful add book's file name to assets.dat
                 File assetsFile = new File(c.getFilesDir(), assetsFileName);
 
-                try {
+                try { //write bookFileName to assets.dat
                     FileOutputStream assetsWriter = new FileOutputStream(assetsFile, true);
                     byte[] tempArray = appendName.getBytes();
 
@@ -110,38 +128,72 @@ public class DocGrabber extends AppCompatActivity {
 
                     editText.setText(onSuccess);
                 } catch (Exception e) {
-                    editText.setText(assetError);
+                    editText.setText(assetError); //This message will trigger if writing to the assets.dat file fails
                 }
 
 
             } else {
-                editText.setText(onFail);
+                editText.setText(onFail); //If book was not found in downloads or any other false return for the import occurs, this will trigger
             }
         }
         else{
-
+            //Messages are already placed above, this else is not necessary for functionality
         }
     }
 
-
+    /**
+     * 2nd function call for importing a book
+     * Creates a File object based on the provided bookFileName from a file in the downloads folder for use in the next function call
+     *
+     *
+     *
+     * @param bookFileName the string representing the file name of the desired book
+     * @param editText the EditText from the associated xml based display
+     *
+     *
+     * @author Brandon Barnes
+     *
+     * @return function returns a boolean which will be true if the book was successfully copied and false otherwise
+     */
     boolean getDocumentFromDownload(String bookFileName, EditText editText) {
         //Get filepath to downloads
         File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
 
         try {
-            ContextWrapper c = new ContextWrapper(getApplicationContext());
+            ContextWrapper c = new ContextWrapper(getApplicationContext()); //Needed for writeFileToAppDirectory
             File bookFile = new File(path, bookFileName); //Make file from ,txt/.pdf in downloads
             //Get File from download folder using path above
 
             //Call the writing function
             return writeFileToAppDirectory(bookFile, bookFileName, c, editText);
-        } catch (Exception e) {
+        } catch (Exception e) { //Will File object cannot be created
 
-            editText.setText(e.getMessage());
+            editText.setText(e.getMessage()); //Not currently visible to user, must dummy out fail message in onEnter to view
             return false;
         }
     }
 
+
+
+
+
+
+    /**
+     * 3rd function call for importing a book
+     * Copies the book associated with bookFile to the local directory
+     *
+     *
+     *
+     * @param bookFileName the string representing the file name of the desired book
+     * @param editText the EditText from the associated xml based display
+     * @param bookFile A File object that is associated with the desired file in the downloads folder
+     * @param c The context for the current activity
+     *
+     *
+     * @author Brandon Barnes
+     *
+     * @return function returns a boolean which will be true if the book was successfully copied and false otherwise
+     */
     boolean writeFileToAppDirectory(File bookFile, String bookFileName, Context c, EditText editText) {
         try {
             File localBook = new File(c.getFilesDir(), bookFileName); //For creating bookFile in local directory
@@ -175,15 +227,28 @@ public class DocGrabber extends AppCompatActivity {
             }
 
         } catch (Exception e) {
-
-            editText.setText(e.getMessage());
+        //Triggers if File object, FileReader, or FileWriter cannot be created
+            editText.setText(e.getMessage());//Not currently visible to user, must dummy out fail message in onEnter to view
             return false;
         }
         return true;
     }
 
+    //This was created for use by fellow developers who need access to the local directory
+    /**
+     * Returns a file with the same name as the String from the local directory
+     *
+     *
+     *
+     *
+     * @param name String which holds the filename to pull from local directory
+     *
+     *
+     * @author Brandon Barnes
+     *
+     * @return A File object for the file in the local directory if successful, otherwise returns null
+     */
 
-    //Returns a file with the same name as the String from the local directory
     File readFileFromDirectory(String name) {
         ContextWrapper c = new ContextWrapper(this);
         try {
